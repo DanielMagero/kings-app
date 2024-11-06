429652c7c6a42986280e38527728dca85ebd89b9
DanielMagero <mdmagero700@gmail.com>
2024-11-06 14:58:44 +0300

    update

--- app/components/Body.js
+++ app/components/Body.js
@@ -2,6 +2,10 @@ function Body(props) {
   return (
     <div>
       <p>{props.details}</p>
+      <p>
+        There are multiple levels of command. The king of the pride has rule
+        over all his entire {props.word} horde.
+      </p>
     </div>
   );
 }
